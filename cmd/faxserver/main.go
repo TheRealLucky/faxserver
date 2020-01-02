f83140a78fdc263291033e70c5790db63b957cba
Patrick Krukenfellner <krukenfellner.patrick@student.htlwrn.ac.at>
2020-01-02 15:13:02 +0100

    implement database connection

--- cmd/faxserver/main.go
+++ cmd/faxserver/main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"../../pkg/config"
 	log "../../pkg/logger"
+	"database/sql"
 )
+
+var Config *config.Configuration
+var Database *sql.DB
 
 func main() {
 	log.Info("starting application")
-	config.InitConfig("conf.yaml")
+	Config, err := config.InitConfig("conf.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	Database, err := config.InitDatabase(Config)
+	if err != nil {
+		panic(err)
+	}
+
+	// testing purposes
+	err = Database.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,9 @@ package config
 import (
 	log "../logger"
 	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
@@ -21,33 +24,66 @@ type Configuration struct {
 	}
 }
 
-var Database *sql.DB
-var Config *Configuration
-
-func InitConfig(filename string) {
+// return a configuration struct, initialized with the values of the given config file.
+func InitConfig(filename string) (*Configuration, error) {
 	log.Info("initializing config")
-	Config = &Configuration{}
+	config := &Configuration{}
 
 	yamlFile, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Error("failed to read yaml file, filename: %v", err)
+		return nil, err
 	}
 
-	err = yaml.Unmarshal(yamlFile, Config)
+	err = yaml.Unmarshal(yamlFile, config)
 	if err != nil {
 		log.Error("failed to unmarshal yaml file: %v", err)
+		return nil, err
 	}
 
 	log.Info("configuration initialized, configured values are:")
 	log.Info("--- config.yaml ---")
-	log.Info("interval: %v", Config.Interval)
-	log.Info("mailboxes: %v", Config.Mailboxes)
-	log.Info("debug: %v", Config.Debug)
-	log.Info("maxRoutines: %v", Config.MaxRoutines)
-	log.Info("database host: %v", Config.Database.Host)
-	log.Info("database port: %v", Config.Database.Port)
-	log.Info("database user: %v", Config.Database.User)
-	log.Info("database password: %v", Config.Database.Password)
-	log.Info("database name: %v", Config.Database.DBName)
+	log.Info("interval: %v", config.Interval)
+	log.Info("mailboxes: %v", config.Mailboxes)
+	log.Info("debug: %v", config.Debug)
+	log.Info("maxRoutines: %v", config.MaxRoutines)
+	log.Info("database host: %v", config.Database.Host)
+	log.Info("database port: %v", config.Database.Port)
+	log.Info("database user: %v", config.Database.User)
+	log.Info("database password: %v", config.Database.Password)
+	log.Info("database name: %v", config.Database.DBName)
 	log.Info("--- config.yaml ---")
+
+	return config, nil
+}
+
+// returns a database, initialized with the values of the given configuration.
+// furthermore opens a connection to this Database.
+func InitDatabase(config *Configuration) (*sql.DB, error) {
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.Database.Host, config.Database.Port, config.Database.User, config.Database.Password, config.Database.DBName)
+	db, err := connectDB(connString)
+
+	if err != nil {
+		log.Error("failed to connect to database: ", err)
+	}
+	return db, nil
+}
+
+func connectDB(connString string) (*sql.DB, error) {
+	log.Info("connecting to database with connection string: %v", connString)
+
+	db,err:= sql.Open("postgres", connString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, errors.Errorf("failed to establish database connection: \n%v", err)
+	} else {
+		log.Info("database connected")
+		return db, nil
+	}
+
 }
