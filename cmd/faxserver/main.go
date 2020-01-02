2acc34017f64fc31660f45e483332507dfca6d92
Patrick Krukenfellner <krukenfellner.patrick@student.htlwrn.ac.at>
2020-01-02 14:48:37 +0100

    implement config initialization

--- cmd/faxserver/main.go
+++ cmd/faxserver/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"../../pkg/config"
 	log "../../pkg/logger"
 )
 
 func main() {
 	log.Info("starting application")
+	config.InitConfig("conf.yaml")
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,14 +3,51 @@ package config
 import (
 	log "../logger"
 	"database/sql"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
-type Config struct {
-
+type Configuration struct {
+	Interval 	int		`yaml:"interval"`
+	Mailboxes 	int		`yaml:"mailboxes"`
+	Debug		bool	`yaml:"debug"`
+	MaxRoutines int		`yaml:"maxRoutines"`
+	Database struct {
+		Host 		string `yaml:"host"`
+		Port 		string `yaml:"port"`
+		User 		string `yaml:"user"`
+		Password 	string `yaml:"password"`
+		DBName 		string `yaml:"dbname"`
+	}
 }
 
 var Database *sql.DB
+var Config *Configuration
 
-func InitConfig() {
+func InitConfig(filename string) {
 	log.Info("initializing config")
+	Config = &Configuration{}
+
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Error("failed to read yaml file, filename: %v", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, Config)
+	if err != nil {
+		log.Error("failed to unmarshal yaml file: %v", err)
+	}
+
+	log.Info("configuration initialized, configured values are:")
+	log.Info("--- config.yaml ---")
+	log.Info("interval: %v", Config.Interval)
+	log.Info("mailboxes: %v", Config.Mailboxes)
+	log.Info("debug: %v", Config.Debug)
+	log.Info("maxRoutines: %v", Config.MaxRoutines)
+	log.Info("database host: %v", Config.Database.Host)
+	log.Info("database port: %v", Config.Database.Port)
+	log.Info("database user: %v", Config.Database.User)
+	log.Info("database password: %v", Config.Database.Password)
+	log.Info("database name: %v", Config.Database.DBName)
+	log.Info("--- config.yaml ---")
 }
