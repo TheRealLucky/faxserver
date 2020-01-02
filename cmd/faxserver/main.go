34c876ea3f44eb362e96291aacddd49b66e8fa8d
Patrick Krukenfellner <krukenfellner.patrick@student.htlwrn.ac.at>
2020-01-02 19:44:04 +0100

    implement semaphore

--- cmd/faxserver/main.go
+++ cmd/faxserver/main.go
@@ -5,14 +5,36 @@ import (
 	"../../pkg/loader"
 	log "../../pkg/logger"
 	"../../pkg/tiffer"
+	"context"
 	"database/sql"
 	"fmt"
+	"golang.org/x/sync/semaphore"
+	"time"
 )
 
 var Config *config.Configuration
 var Database *sql.DB
 
 func main() {
+	sem := semaphore.NewWeighted(10)
+	ctx := context.TODO()
+
+	for i := 0; i < 100; i++ {
+		// When maxWorkers goroutines are in flight, Acquire blocks until one of the
+		// workers finishes.
+		if err := sem.Acquire(ctx, 1); err != nil {
+			log.Error("Failed to acquire semaphore: %v", err)
+			break
+		}
+
+		go func(i int) {
+			fmt.Println("fuck me", i)
+			time.Sleep(2 * time.Second)
+			defer sem.Release(1)
+			//out[i] = collatzSteps(i + 1)
+		}(i)
+	}
+
 	log.Info("starting application")
 	Config, err := config.InitConfig("conf.yaml")
 	if err != nil {
@@ -42,7 +64,7 @@ func main() {
 	fmt.Println(account)
 	*/
 	log.Info("getting user uuid")
-	uuid, domain, err := loader.Get_user_uuid(Database, )
+	//uuid, domain, err := loader.Get_user_uuid(Database, )
 
 	tiffer.Create_folder("x","u")
 	m := make(map[string]string)
