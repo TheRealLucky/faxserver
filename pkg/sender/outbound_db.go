8c03bb9b1ba411063029f766b5fa897698ba97b2
Patrick Krukenfellner <krukenfellner.patrick@student.htlwrn.ac.at>
2020-01-03 16:22:54 +0100

    combine dialplan and dialplanInfo database functions to one

--- pkg/sender/outbound_db.go
+++ pkg/sender/outbound_db.go
@@ -1,48 +1,69 @@
 package sender
 
+
 import (
 	log "../logger"
 	"database/sql"
-	"github.com/pkg/errors"
 )
 
-type DialPlanInfo struct {
+type DialPlan struct {
 	UUID		string
+	Continue	string
+	Info		[]DialPlanInfo
+}
+
+type DialPlanInfo struct {
 	DetailTag	string
 	DetailType	string
-	Continue	string
+	DetailData  string
 }
 
-type DialPlanDetailsInfo struct {
-	DetailsTag  string
-	DetailsType string
-	DetailsData string
-}
+func GetDialPlans(db *sql.DB, domainUUID string) ([]DialPlan, error) {
+	var result []DialPlan
 
-func GetDialPlanInfo(db *sql.DB, domainUUID string) ([]DialPlanInfo, error) {
-	var result []DialPlanInfo
-
-	query := "select dialplan_uuid, dialplan_detail_tag, dialplan_detail_type, dialplan_continue " +
-		"       from v_dialplans " +
-		"      where (domain_uuid = $1 or domain_uuid is null) " +
-		"        and app_uuid='8c914ec3-9fc0-8ab5-4cda-6c9288bdc9a3' " +
-		"        and dialplan_enabled = 'true' " +
+	query := "select dialplan_uuid, dialplan_continue" +
+		"       from v_dialplans" +
+		"      where (domain_uuid = '$1' or domain_uuid is null)" +
+		"        and app_uuid='8c914ec3-9fc0-8ab5-4cda-6c9288bdc9a3'" +
+		"        and dialplan_enabled = 'true'" +
 		"      order by dialplan_order asc"
 
 	res, err := db.Query(query, domainUUID)
 	if err != nil {
-		log.Error("failed to perform query")
-		return nil, errors.Errorf("[outbound_route_to_bridge] failed to get dialplan infos \n%v", err)
+		log.Error("failed to execute query")
+		return nil, err
 	}
 
 	for res.Next() {
-		info := DialPlanInfo{}
-		err = res.Scan(&info.UUID, &info.DetailTag, &info.DetailType, & info.Continue)
+		plan := DialPlan{}
+		plan.Info = make([]DialPlanInfo, 0)
+
+		err = res.Scan(&plan.UUID, &plan.Continue)
 		if err != nil {
 			log.Error("failed to scan result set")
 			return nil, err
 		}
-		result = append(result, info)
+
+		query = "select dialplan_detail_tag, dialplan_detail_type, dialplan_detail_data" +
+			"      from v_dialplan_details" +
+			"     where dialplan_uuid = '$1'"
+
+		newRes, err := db.Query(query, plan.UUID)
+		if err != nil {
+			log.Error("failed to execute query")
+			return nil, err
+		}
+
+		for newRes.Next() {
+			info := DialPlanInfo{}
+			err = newRes.Scan(&info.DetailTag, &info.DetailType, &info.DetailData)
+			if err != nil {
+				log.Error("failed to scan result set")
+				return nil, err
+			}
+			plan.Info = append(plan.Info, info)
+		}
+		result = append(result, plan)
 	}
 	return result, nil
 }
